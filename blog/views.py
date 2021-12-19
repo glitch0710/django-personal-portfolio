d86bb2e46d0e0c3c640e3d2f11e92ab1409717d3
Rhogiel Andoy <theglitch0710@gmail.com>
2021-12-19 21:59:32 +0800

    Showing blog details

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Blog
 
 
 def all_blogs(request):
-    blogs = Blog.objects.order_by('-post_date')
+    blogs = Blog.objects.order_by('-post_date')[:5]
     count = Blog.objects.count()
 
     if count > 1:
@@ -12,3 +12,8 @@ def all_blogs(request):
         count = str(count) + ' blog'
 
     return render(request, 'blog/all_blogs.html', {'blogs': blogs, 'count': count})
+
+
+def blog_details(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/details.html', {'blog': blog})
