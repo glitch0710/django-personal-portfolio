fa16fd2e3ed78d7b185dec59bc66b6466676dc02
Rhogiel Andoy <theglitch0710@gmail.com>
2021-12-19 15:04:37 +0800

    Creating Blogs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    post_title = models.CharField(max_length=100)
+    post_date = models.DateField(auto_now_add=True)
+    post_desc = models.TextField()
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from blog.models import Blog
 
-# Create your views here.
+
+def all_blogs(request):
+    blogs = Blog.objects.order_by('-post_date')
+    count = Blog.objects.count()
+
+    if count > 1:
+        count = str(count) + ' blogs'
+    else:
+        count = str(count) + ' blog'
+
+    return render(request, 'blog/all_blogs.html', {'blogs': blogs, 'count': count})
